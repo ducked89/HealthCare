be393c57754eecf230361e6cdd15f669722c118a
Jean Duke Dorcy <dorcyd89@gmail.com>
2021-06-23 10:17:12 -0500

    Local data to work on api

---
+++ src/graphql/resolvers/data.js
@@ -0,0 +1,56 @@
+const settings = {
+  id: "hr6",
+  companies: [
+    {
+      id: "sf",
+      name: "Picpharma",
+      title: "Picpharma",
+      businessId: "Picpharma-098689",
+      businessType: "Medical",
+      logo: "",
+      industry: "Health",
+      contact: "Test company",
+      email: "picpharma@gmai.com",
+      phone: "23578903",
+      website: "picpharma.com",
+      address: {
+        street: "fsh;sdf  sfjhslkjfs kjsh s",
+        city: "sdfgsjh sf kjfha ealkl vakjk",
+        postal: "2367",
+        state: "OU",
+        country: "Haiti",
+      },
+      parent: "",
+      children: [],
+      status: "Active",
+    },
+  ],
+  author: {
+    name: "Code Handler",
+    description: `Code Handler is an agency operating in the web trades.
+      We offer our services for all types of internet projects: showcase site, tailor-made platform,
+      android or IOS smartphone and desktop applications, e-commerce site and marketplace ... We also
+      support startups in their development as technical support for realization of their projects:
+      marketplace style Airbnb, Uber ... as well as any other innovative project.`,
+    email: "codehnd@gmail.com",
+    website: "https://www.codehandler.io",
+    phone: "(509)4287 2495",
+    address: {
+      street: "32b, Ave Johnbrown",
+      city: "Port-au-Prince",
+      postal: "123456",
+      state: "OU",
+      country: "Haiti",
+    },
+    socials: {
+      youtube: "youtube.com/codehnd",
+      facebook: "facebook.com/codehnd",
+      twitter: "twitter.com/codehnd",
+      instagram: "instagram.com/codehnd",
+      linkedin: "linkedin.com/codehnd",
+      github: "github.com/codehnd",
+    },
+  },
+};
+
+module.exports = settings;

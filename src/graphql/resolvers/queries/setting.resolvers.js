747a5cd0138bcacf19de6397623e1a558a193699
Jean Duke Dorcy <dorcyd89@gmail.com>
2021-06-23 16:12:30 -0500

    Fix to find everything in settings array

--- src/graphql/resolvers/queries/setting.resolvers.js
+++ src/graphql/resolvers/queries/setting.resolvers.js
@@ -3,8 +3,18 @@ const settings = require("../data");
 module.exports = {
   Query: {
     settings: () => settings,
-    company: (parent, { id }, ctx, info) =>
-      settings.companies.find((company) => id === company.id),
-    companies: () => settings.companies,
+    company: (parent, { id }, ctx, info) => {
+      const setting = settings.find((sett) => {
+        const comp = sett.companies.filter((company) => id === company.id);
+        return comp;
+      });
+      return setting.companies.find((comp) => comp.id === id);
+    },
+    companies: () => {
+      const obj = settings.find((sett) => {
+        return sett.companies;
+      });
+      return obj.companies;
+    },
   },
 };
